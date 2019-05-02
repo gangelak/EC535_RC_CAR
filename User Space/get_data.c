a0621484c8de429569c341dccffffee21d9a75bc
Ioannis Angelakopoulos <10489837+gangelak@users.noreply.github.com>
2019-05-01 22:05:22 -0400

    Added support for the self driving module

--- User Space/get_data.c
+++ User Space/get_data.c
@@ -5,8 +5,15 @@
 #include <unistd.h>
 #include <signal.h>
 
+/*Bluetooth file - rfcomm0*/
 int bl_file;
+/*Remote controlled module file - mycar*/
 int car_file;
+/*Self navigating module file - autocar*/
+int auto_file;
+/*Flag to indicate if we are in the self driving module*/
+int auto_mode = 0;
+
 void sighandler(int);
 
 int main (int argc, char *argv){
@@ -16,9 +23,7 @@ int main (int argc, char *argv){
 	 * In an infinite while loop the module will try to read
 	 * data from the rfcomm file (if it exists) and then copy
 	 * them to the car kernel module
-	 *
-	 *
-	 * */
+	 */
 	
 	struct sigaction action;
 	
@@ -30,21 +35,6 @@ int main (int argc, char *argv){
 
 
 	char line[256];
-	
-	bl_file = open("/dev/rfcomm0",O_RDONLY);
-	
-	if (bl_file == -1){
-		printf("No bluetooth connection\n");
-		continue;
-	}
-	
-	// We can receive data from bluetooth. Pass them to the car kernel module
-	car_file = open("/dev/mycar",O_RDWR);
-	
-	if (car_file == -1){
-		printf("Something is wrong with the car kernel module\n");
-		return 1;
-	}
 
 	while (1){
 		
@@ -59,10 +49,20 @@ int main (int argc, char *argv){
 		}
 		
 		if( access( "/dev/mycar", F_OK ) == -1 ) {
-			bl_file = open("/dev/mycar",O_RDONLY);
+			bl_file = open("/dev/mycar",O_RDWR);
 			
 			if (bl_file == -1){
 				printf("Something is wrong with the car kernel module\n");
+				continue;
+			}
+    			
+		}
+		
+		if( access( "/dev/autocar", F_OK ) == -1 ) {
+			auto_file = open("/dev/autocar",O_RDWR);
+			
+			if (auto_file == -1){
+				printf("Something is wrong with the autocar kernel module\n");
 				continue;
 			}
     			
@@ -72,15 +72,31 @@ int main (int argc, char *argv){
 		// Transfer data character by character
 		memset(line,'\0',256);
 		while(i = (read(bl_file,line,256)) != 0){
-			//printf("%c\n",line[0]);
-			write(car_file,line,1);
-			memset(line,'\0',256);
+			//Initiate self driving mode
+			if (line[0] == 'N'){
+				write(auto_file,line,1);
+				memset(line,'\0',256);
+				auto_mode = 1;
+				
+			}
+			else if (line[0] == 'B'){
+				write(auto_file,line,1);
+				memset(line,'\0',256);
+				auto_mode = 0;
+				
+			}
+			//Remote controlled mode
+			else{
+				if (auto_mode == 0){
+					write(car_file,line,1);
+					memset(line,'\0',256);
+				}
+			}
 		}
-
-	
 	}
 	
-
+	
+	close(auto_file);
 	close(car_file);
 	close(bl_file);
 	return 0;
@@ -91,6 +107,7 @@ void sighandler(int signo){
 	if (signo == SIGINT){
 		close(bl_file);
 		close(car_file);
+		close(auto_file);
 		exit(0);
 	}
 }
